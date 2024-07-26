ed0f0beb09f8a9b66f58e1995e7e02f9b1fec0dd
sukhman-12 <141357313+sukhman-12@users.noreply.github.com>
2024-07-26 14:58:30 -0600

    first commit

---
+++ app/resume/page.js
@@ -0,0 +1,50 @@
+export default function Resume() {
+    return (
+      <div>
+        <h1>Resume</h1>
+        <section>
+          <h2>Contact</h2>
+          <p>32 RED EMBERS TERRCE NE, CALGARY, ALBERTA</p>
+          <p>Phone: 587-896-7704</p>
+          <p>Email: Ksukhman961@gmail.com</p>
+        </section>
+        <section>
+          <h2>Education</h2>
+          <p>Southern Alberta Institute of Technology, Calgary, Alberta</p>
+          <p>Diploma in Software Technology</p>
+        </section>
+        <section>
+          <h2>Key Skills</h2>
+          <ul>
+            <li>Travel coordination</li>
+            <li>Clerical support</li>
+            <li>File management</li>
+            <li>Bookkeeping</li>
+            <li>Data Entry</li>
+            <li>Administrative support</li>
+          </ul>
+        </section>
+        <section>
+          <h2>Objective</h2>
+          <p>
+            Ambitious Receptionist equipped to handle front desk operations greeting guests, routing correspondence, and solving immediate issues. Goal-oriented and meticulous professional with outstanding computer skills and telephone etiquette. Committed to contributing to company growth. Focused on providing professional service and support to every office visitor. Diligent about scheduling appointments, managing packages, and routing mail to recipients. Positive nature with excellent people skills.
+          </p>
+        </section>
+        <section>
+          <h2>Experience</h2>
+          <p>June 2023 - Present: Cashier at Chalo Freshco, Cornerstone</p>
+          <p>July 2023 - Present: Receptionist at Stonegate Physio and Massage Clinic, Airdrie</p>
+          <p>Responsibilities: Overseeing daily operations, managing staff, ensuring compliance with banking regulations, and providing exceptional customer service. Also responsible for analyzing financial data, identifying trends, and developing strategies to improve the bank's performance.</p>
+        </section>
+        <section>
+          <h2>Communication</h2>
+          <p>Implemented new procedures and technologies that improved efficiency and streamlined operations.</p>
+        </section>
+        <section>
+          <h2>Leadership</h2>
+          <p>Successfully led a team to exceed sales goals while maintaining excellent customer satisfaction scores.</p>
+        </section>
+      </div>
+    );
+  }
+  
